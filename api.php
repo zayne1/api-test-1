d8360948a1baf6dba78c035860afdaeb4c2e3fa7
ace <ace@ace-lap>
2013-11-09 11:31:02 +0200

    Prelim planning - trying to flesh out how we will approach this

--- api.php
+++ api.php
@@ -1,55 +1,61 @@
 <?php
-echo 'x';
-var_dump($_SERVER['REQUEST_URI']);
-$request_parts = explode('/', $_SERVER['REQUEST_URI']);
-var_dump($request_parts);
-$file_type     = $_GET['type'];
-var_dump($file_type);
-echo basename($_SERVER['REQUEST_URI']);
-echo '---'.$request_parts['user'];
-echo '==='.$_SERVER['REQUEST_METHOD'];
+/*
+ * Big idea: we spilt the system into 3 main sectiont: RESTful application in front, API Class behind that, and DB class behind that.
+ * 
+ * 
+ * API class
+ * 		user
+ * 			register
+ * 			authenticate
+ * 			add
+ *	 		view
+ *			del (low prio)
+ * 			update (low prio)
+ * 			list all (low prio) 
+ 
+ * 			
+ * 		topics
+ * 			add
+ * 			view
+ *	 		del (low prio)
+ * 			update (low prio)
+ * 			list all (low prio)
+ * 				comments
+ * 					add
+ *	 				view
+ * 					delete
+ * 						reply
+ * 							view
+ * 							add
+ * 
+ *
+ * JSON_Util class
+ * 		get_request_method($data) // dynamically named. uses 
+ * 		put_request_method($data) // dynamically named
+ * 		post_request_method($data) // dynamically named 
+ * 		del_request_method($data) // dynamically named
+ * 		process_request(request method $request_method)
+ * 	
+ * 
+ * 
+ * 
+ * psuedo code:
+ * 	if we have request data, 
+ * 		get the url ( eg http://app/users/register )
+ * 		instead of using a big switch case of url mappings to decide what to do, lets map our php class objects and methods to mirror our urls. ie:
+ * 			    http://app/users/register will map to $Users->register(). Try to use dynamically named vars.
+ * 		if req_method = POST, run the add method of the current 'collection' url (the first segment, eg user)
+ * 			remember that user/register on the url must map to user/add in backend
+ * 			user/authenticate = post user name and password to /user, and recieve back a user exists msg   
+ * 
+ * if no request data
+ *		if req_method = GET,
+ * 			if more than 2 url params are loaded, bomb out with err
+ * 			if only a collection url is loaded, return the 'list all'
+ *	 		if we have 2 urls
+ * 					view(item)
+ * 		
+ * */
 
-switch($request_parts) {
-    case ($request_parts[2] == 'user'):
-        echo 'zzz';
-        break;
-    case ($request_parts[2] == 'man'):
-        echo 'yyy';
-        break;
-    default:
-        echo $request_parts;
-}
-
-$request_method = $_SERVER['REQUEST_METHOD'];
-echo '*************'.$request_method;
-$data = array();
-switch ($request_method)  
-    {  
-        case 'GET':  
-            $data = $_GET;  
-            break;  
-        case 'POST':  
-            $data = $_POST;  
-            break;  
-        case 'PUT':  
-            parse_str(file_get_contents('php://input'), $put_vars);
-            $data = $put_vars;  
-            break;  
-    }  
-	
-
-	echo var_dump($data);
-	
-	echo '\r\n<br>';
-	echo '\r\n<br>';
-	// $x = json_decode($data);
-	// echo var_dump($x);
-	echo $data['name'];
-	
-	echo '\r\n<br>';
-	echo '\r\n<br>';
-	echo var_dump($_POST);
-	
-	// echo 'DATA2:'. ($data['name']);
 
 ?>
