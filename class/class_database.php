b143e503b061be2460c6e81cdfdfb56a26424093
zayne <zayne.a@gmail.com>
2014-08-18 23:08:48 +0200

    Documentation format change for database class

--- class/class_database.php
+++ class/class_database.php
@@ -30,10 +30,11 @@ class Database {
         return $this->_numRows;
     }
     
-    /*  After a select query is run, we need to be able to get the num of resultant data rows. This fun will return 
-     *  an array of that resultant info. To use it, call it directly after using "this->DB->query($sql)". Have a 
-     *  look at api_iprospect's getUsers() method to see it in use
-     */
+    /**
+     * After a select query is run, we need to be able to get the num of resultant data rows. This fun will return 
+     * an array of that resultant info. To use it, call it directly after using "this->DB->query($sql)". Have a 
+     * look at api_iprospect's getUsers() method to see it in use
+     */     
     public function rows() {
         $rows = array();
         for ($i=0; $i < $this->numRows(); $i++) { 
